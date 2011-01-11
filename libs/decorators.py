dd28256377a1086eec0e1aa4d4f6842b8460946a
Dmitri Patrakov <traditio@gmail.com>
2011-01-11 10:56:31 +0300

    fixes error in hamlish and adds ajax_response decorator

--- libs/decorators.py
+++ libs/decorators.py
@@ -1,7 +1,12 @@
 #coding=utf-8
 #--- Author: Dmitri Patrakov <traditio@gmail.com>
-from coffin.shortcuts import render_to_response
+import logging
+
 from django.template.context import RequestContext
+
+from annoying.decorators import JsonResponse
+from coffin.shortcuts import render_to_response
+
 
 try:
     from functools import wraps
@@ -32,3 +37,27 @@ def render_to(template=None, mimetype="text/html"):
                         context_instance=RequestContext(request), mimetype=mimetype)
         return wrapper
     return renderer
+
+
+def ajax_request(func):
+    """
+    If view returned serializable dict, returns JsonResponse with this dict as content.
+
+    example:
+
+        @ajax_request
+        def my_view(request):
+            news = News.objects.all()
+            news_titles = [entry.title for entry in news]
+            return {'news_titles': news_titles}
+    """
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        try:
+            response = func(request, *args, **kwargs)
+        except Exception, e:
+            logging.error('AJAX - %s raises %s: %s', func.func_name, str(e.__class__), e.message)
+            return JsonResponse({'status': 'error', 'response': '%s: %s' % (str(e.__class__), e.message)})
+        else:
+            return JsonResponse({'status': 'ok', 'response': response})
+    return wrapper
